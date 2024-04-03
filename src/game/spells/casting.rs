6e10a6eb8c826e94a8f1376da59d1d28dc13baee
izzymg <izzymg@proton.me>
2024-04-03 18:27:15 +1300

    clean up cast finish check

--- src/game/spells/casting.rs
+++ src/game/spells/casting.rs
@@ -1,12 +1,22 @@
-use std::time::Duration;
-use bevy::{ecs::{
-        component::Component, entity::Entity, event::{Event, EventReader, EventWriter}, system::{Commands, In, Query, Res}
-    }, log::*, time::{Time, Timer}
+use bevy::{
+    ecs::{
+        component::Component,
+        entity::Entity,
+        event::{Event, EventReader, EventWriter},
+        system::{Commands, In, Query, Res},
+    },
+    log::*,
+    time::{Time, Timer},
 };
+use std::time::Duration;
 
 use crate::game::alignment::{self, Hostility};
 
-use super::{resource, spell_application::{self, SpellApplicationEvent}, SpellID};
+use super::{
+    resource,
+    spell_application::{self, SpellApplicationEvent},
+    SpellID,
+};
 
 #[derive(Event)]
 pub struct StartCastingEvent {
@@ -18,7 +28,9 @@ pub struct StartCastingEvent {
 impl StartCastingEvent {
     pub fn new(entity: Entity, target: Entity, spell_id: SpellID) -> Self {
         Self {
-            entity, target, spell_id,
+            entity,
+            target,
+            spell_id,
         }
     }
 }
@@ -33,7 +45,11 @@ pub(super) struct CastingSpell {
 
 impl CastingSpell {
     fn new(spell_id: SpellID, target: Entity, cast_time: Duration) -> CastingSpell {
-        CastingSpell { spell_id, target, cast_timer: Timer::new(cast_time, bevy::time::TimerMode::Once) }
+        CastingSpell {
+            spell_id,
+            target,
+            cast_timer: Timer::new(cast_time, bevy::time::TimerMode::Once),
+        }
     }
 }
 
@@ -45,56 +61,63 @@ pub(super) fn check_finished_casts_system(
     mut spell_app_ev_w: EventWriter<spell_application::SpellApplicationEvent>,
 ) {
     for (entity, casting, faction_member) in query.iter_mut() {
-        if casting.cast_timer.finished() {
-            commands.entity(entity).remove::<CastingSpell>();
+        if !casting.cast_timer.finished() {
+            continue;
+        }
+        commands.entity(entity).remove::<CastingSpell>();
 
-            let spell = spell_list.get_spell_data(casting.spell_id).unwrap();
-
-            // no faction check if entity casts a helpful spell on itself
-            if !(spell.hostility == Hostility::Friendly && entity == casting.target) {
-                if let Some(caster_faction) = faction_member {
-                    let target_faction = faction_checker.get_entity_faction(casting.target).unwrap_or_default();
-                    if !alignment::is_valid_target(spell.hostility,  caster_faction.0, target_faction) {
-                        // skip invalid
-                        continue;
-                    }
-                } else if spell.hostility == Hostility::Friendly {
-                    // skip friendly-other casts when caster has no faction
-                    continue;
-                }
-            }
+        let mut push = || {
             // send spell application events
             spell_app_ev_w.send(SpellApplicationEvent {
                 origin: entity,
                 spell_id: casting.spell_id,
                 target: casting.target,
             });
+        };
+
+        let spell = spell_list.get_spell_data(casting.spell_id).unwrap();
+        let is_selfcast = entity == casting.target;
+        if is_selfcast && spell.hostility == Hostility::Friendly {
+            // allow friendly self-casts immediately
+            push();
+        }
+
+        if let Some(caster_faction) = faction_member {
+            let target_faction = faction_checker
+                .get_entity_faction(casting.target)
+                .unwrap_or_default();
+            if alignment::is_valid_target(spell.hostility, caster_faction.0, target_faction) {
+                // allow valid
+                push();
+            }
+        } else if spell.hostility == Hostility::Hostile {
+            // allow hostile when factionless
+            push();
         }
     }
 }
 
 // Tick spell casts and push finished casts to spell application.
-pub(super) fn tick_cast_system(
-    time: Res<Time>,
-    mut query: Query<&mut CastingSpell>,
-) {
+pub(super) fn tick_cast_system(time: Res<Time>, mut query: Query<&mut CastingSpell>) {
     for mut casting in query.iter_mut() {
         casting.cast_timer.tick(time.delta());
     }
 }
 
-
 /// Begin spell casts when event received.
 pub(super) fn handle_start_casting_event_system(
     mut events: EventReader<StartCastingEvent>,
     mut commands: Commands,
-    spell_list: Res<resource::SpellList>
-)
-{
+    spell_list: Res<resource::SpellList>,
+) {
     for ev in events.read() {
         if let Some(spell) = spell_list.get_spell_data(ev.spell_id) {
             debug!("{:?} starts casting spell {}", ev.entity, spell.name);
-            commands.entity(ev.entity).insert(CastingSpell::new(ev.spell_id, ev.target, spell.cast_time));
+            commands.entity(ev.entity).insert(CastingSpell::new(
+                ev.spell_id,
+                ev.target,
+                spell.cast_time,
+            ));
         } else {
             error!("no spell {}", ev.spell_id);
         }
