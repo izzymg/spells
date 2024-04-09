de3ba5148ad433226db64e021f5f5c7c6a59aadb
izzymg <izzymg@proton.me>
2024-04-09 13:24:07 +1200

    move serialization into shared lib

--- lib_spells/src/lib.rs
+++ lib_spells/src/lib.rs
@@ -1,2 +1,51 @@
 pub const CLIENT_EXPECT: &str = "SPELLCLIENT OK 0.1\n";
 pub const SERVER_HEADER: &str = "SPELLSERVER 0.1\n";
+
+pub mod serialization {
+    use bincode;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Deserialize, Serialize, Debug, Copy, Clone)]
+    pub struct EntityCaster {
+        pub entity: u32,
+        pub timer: u128,
+        pub max_timer: u128,
+        pub spell_id: usize,
+    }
+
+    #[derive(Deserialize, Serialize, Debug, Copy, Clone)]
+    pub struct EntityHealth {
+        pub entity: u32,
+        pub health: i64,
+    }
+
+    #[derive(Deserialize, Serialize, Debug, Copy, Clone)]
+    pub struct EntityAura {
+        pub entity: u32,
+        pub aura_id: usize,
+        pub remaining: u128,
+    }
+
+    #[derive(Deserialize, Serialize, Debug, Default)]
+    pub struct WorldState {
+        pub health: Vec<EntityHealth>,
+        pub casters: Vec<EntityCaster>,
+        pub auras: Vec<EntityAura>,
+    }
+
+    impl WorldState {
+        pub fn serialize(&self) -> Result<Vec<u8>, bincode::ErrorKind> {
+            match bincode::serialize(&self) {
+                Ok(data) => Ok(data),
+                Err(err) => Err(*err),
+            }
+        }
+
+        pub fn deserialize(data: &[u8]) -> Result<WorldState, bincode::ErrorKind> {
+            match bincode::deserialize::<WorldState>(data) {
+                Ok(state) => Ok(state),
+                Err(e) => Err(*e)
+            }
+        }
+    }
+}
--- server/src/game/net/mod.rs
+++ server/src/game/net/mod.rs
@@ -1,21 +1,20 @@
 mod serialize;
 mod socket;
 use std::{sync::mpsc, thread};
-
 use bevy::{app, log, prelude::*, utils::dbg};
-
 use super::components;
+use lib_spells::serialization;
 
-fn sys_create_state(world: &mut World) -> serialize::WorldState {
-    let mut state: serialize::WorldState = default();
+fn sys_create_state(world: &mut World) -> serialization::WorldState {
+    let mut state: serialization::WorldState = default();
 
     state.health = world
         .iter_entities()
         .filter_map(|e| {
             e.get::<components::Health>().and_then(|v| {
-                Some(serialize::EntityHealth {
+                Some(serialization::EntityHealth {
                     health: v.0,
-                    entity: e.id(),
+                    entity: e.id().index(),
                 })
             })
         })
@@ -25,11 +24,11 @@ fn sys_create_state(world: &mut World) -> serialize::WorldState {
         .iter_entities()
         .filter_map(|e| {
             e.get::<components::CastingSpell>().and_then(|v| {
-                Some(serialize::EntityCaster {
+                Some(serialization::EntityCaster {
                     max_timer: v.cast_timer.duration().as_millis(),
                     timer: v.cast_timer.elapsed().as_millis(),
                     spell_id: v.spell_id.get(),
-                    entity: e.id(),
+                    entity: e.id().index(),
                 })
             })
         })
@@ -39,9 +38,9 @@ fn sys_create_state(world: &mut World) -> serialize::WorldState {
     state.auras = world
         .iter_entities()
         .flat_map(|e| aura_query.get(world, e.id()))
-        .map(|(p, a)| serialize::EntityAura {
+        .map(|(p, a)| serialization::EntityAura {
             aura_id: a.id.get(),
-            entity: p.get(),
+            entity: p.get().index(),
             remaining: a.get_remaining_time().as_millis(),
         })
         .collect();
@@ -49,10 +48,10 @@ fn sys_create_state(world: &mut World) -> serialize::WorldState {
     state
 }
 fn sys_broadcast_state(
-    In(world_state): In<serialize::WorldState>,
+    In(world_state): In<serialization::WorldState>,
     mut sender: ResMut<ClientStreamSender>,
     mut exit_events: ResMut<Events<app::AppExit>>,
-) -> serialize::WorldState {
+) -> serialization::WorldState {
     if !sender.send_data(
         world_state
             .serialize()
