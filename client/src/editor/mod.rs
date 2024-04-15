72d9f6d0e0da63db27636aef01b1f8347ccd4c4c
izzymg <izzymhg@gmail.com>
2024-04-15 22:40:28 +1200

    place and remove blocks

--- client/src/editor/mod.rs
+++ client/src/editor/mod.rs
@@ -1,17 +1,54 @@
 use crate::{controls::free_cam, render, GameState, GameStates};
-use bevy::prelude::*;
+use bevy::{input, log, prelude::*};
 
-fn sys_spawn(
-    mut commands: Commands,
-    mut terrain_event_send: EventWriter<render::GenerateTerrainEvent>,
+#[derive(Resource, Default)]
+struct PlacePreview(render::Voxel);
+
+#[derive(Resource, Default)]
+struct EditorTerrain(render::VoxelTerrain);
+
+fn sys_spawn(mut commands: Commands) {
+    commands.spawn((Camera3dBundle::default(), free_cam::FreeCamera::default()));
+}
+
+fn sys_draw_preview_gizmos(
+    mut gizmos: Gizmos,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut place_preview: ResMut<PlacePreview>,
 ) {
-    commands.spawn((
-        Camera3dBundle::default(),
-        free_cam::FreeCamera::default(),
-    ));
+    let (camera, camera_trans) = camera_query.single();
+    let world_space_coords = camera
+        .viewport_to_world(camera_trans, Vec2::ZERO)
+        .unwrap()
+        .origin;
 
-    let terrain = render::VoxelTerrain(vec![render::Voxel(0, 0, 0)]);
-    terrain_event_send.send(render::GenerateTerrainEvent { terrain });
+    place_preview.0 = (world_space_coords + (camera_trans.forward() * 5.0))
+        .round()
+        .into();
+    gizmos.cuboid(
+        Transform::from_translation(place_preview.0.into())
+            .with_scale(Vec3::ONE * render::VOXEL_SIZE as f32),
+        Color::rgba(0.0, 0.8, 0.8, 0.9),
+    );
+}
+
+fn sys_add_terrain(
+    place_preview: ResMut<PlacePreview>,
+    mut editor_terrain: ResMut<EditorTerrain>,
+    mut terrain_event_send: EventWriter<render::GenerateTerrainEvent>,
+    mouse_buttons: Res<input::ButtonInput<input::mouse::MouseButton>>,
+) {
+    if mouse_buttons.just_released(MouseButton::Left) {
+        log::info!("checking: {}", place_preview.0);
+        if let Some(i) = editor_terrain.0.find(place_preview.0) {
+            editor_terrain.0.remove(i);
+        } else {
+            editor_terrain.0.add(place_preview.0);
+        }
+        terrain_event_send.send(render::GenerateTerrainEvent {
+            terrain: editor_terrain.0.clone(), //ew
+        });
+    }
 }
 
 pub struct EditorPlugin;
@@ -19,6 +56,9 @@ impl Plugin for EditorPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(free_cam::FreeCameraPlugin);
         app.world.get_resource_mut::<GameState>().unwrap().0 = GameStates::Game;
+        app.insert_resource(EditorTerrain::default());
+        app.insert_resource(PlacePreview::default());
         app.add_systems(Startup, sys_spawn);
+        app.add_systems(Update, (sys_add_terrain, sys_draw_preview_gizmos));
     }
 }
--- client/src/render/mod.rs
+++ client/src/render/mod.rs
@@ -1,36 +1,117 @@
-use crate::{GameState, GameStates};
 use bevy::{
     ecs::system::SystemParam,
     log,
     prelude::*,
     render::{mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology},
 };
+use std::fmt;
 
-const VOXEL_SIZE: i32 = 1;
+// Size in world space of voxels
+pub const VOXEL_SIZE: i32 = 1;
 
+#[derive(Default, Copy, Clone, PartialEq, Eq)]
 pub struct Voxel(pub i32, pub i32, pub i32);
 
-#[derive(Resource)]
+impl From<(i32, i32, i32)> for Voxel {
+    fn from(v: (i32, i32, i32)) -> Self {
+        Self(v.0, v.1, v.2)
+    }
+}
+
+impl From<Vec3> for Voxel {
+    fn from(value: Vec3) -> Self {
+        Self(value.x as i32, value.y as i32, value.z as i32)
+    }
+}
+
+impl From<Voxel> for Vec3 {
+    fn from(value: Voxel) -> Self {
+        Self::new(value.0 as f32, value.1 as f32, value.2 as f32)
+    }
+}
+
+impl fmt::Display for Voxel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "voxel: {}, {}, {}", self.0, self.1, self.2)
+    }
+}
+
+#[derive(Resource, Default, Clone)]
 pub struct VoxelTerrain(pub Vec<Voxel>);
 
+pub enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+    Forward,
+    Backward,
+}
+
 impl VoxelTerrain {
-    fn neighbor_right(&self, index: usize) -> bool {
-        self.neighbor_x(index, 1)
+
+    pub fn add(&mut self, v: Voxel) {
+        self.0.push(v);
     }
-    fn neighbor_left(&self, index: usize) -> bool {
-        self.neighbor_x(index, -1)
+
+    pub fn remove(&mut self, index: usize) -> Voxel {
+        self.0.remove(index)
     }
+
+    pub fn find(&self, pos: Voxel) -> Option<usize> {
+        for (i, v) in self.0.iter().enumerate() {
+            if *v == pos {
+                return Some(i);
+            }
+        }
+        None
+    }
+
+    pub fn has_neighbor(&self, index: usize, dir: Direction) -> bool {
+        match dir {
+            Direction::Up => self.neighbor_y(index, 1),
+            Direction::Down => self.neighbor_y(index, -1),
+            Direction::Left => self.neighbor_x(index, -1),
+            Direction::Right => self.neighbor_x(index, 1),
+            Direction::Forward => self.neighbor_z(index, 1),
+            Direction::Backward => self.neighbor_z(index, -1),
+        }
+    }
+
     fn neighbor_x(&self, index: usize, dir: i32) -> bool {
         let uv = self.0.get(index).unwrap();
         self.0
             .iter()
-            .filter(|v| if dir < 0 { v.0 < uv.0 } else { v.0 > uv.0 })
+            .filter(|v| {
+                let in_range = if dir < 0 { v.0 < uv.0 } else { v.0 > uv.0 };
+                in_range && (v.1 == uv.1 && v.2 == uv.2)
+            })
             .any(|v| v.0 - uv.0 == dir)
     }
+    fn neighbor_y(&self, index: usize, dir: i32) -> bool {
+        let uv = self.0.get(index).unwrap();
+        self.0
+            .iter()
+            .filter(|v| {
+                let in_range = if dir < 0 { v.1 < uv.1 } else { v.1 > uv.1 };
+                in_range && (v.0 == uv.0 && v.2 == uv.2)
+            })
+            .any(|v| v.1 - uv.1 == dir)
+    }
+    fn neighbor_z(&self, index: usize, dir: i32) -> bool {
+        let uv = self.0.get(index).unwrap();
+        self.0
+            .iter()
+            .filter(|v| {
+                let in_range = if dir < 0 { v.2 < uv.2 } else { v.2 > uv.2 };
+                in_range && (v.0 == uv.0 && v.1 == uv.1)
+            })
+            .any(|v| v.2 - uv.2 == dir)
+    } 
 }
 
 #[derive(Component)]
-struct CustomUV;
+struct VoxelEntity;
 
 fn create_quad() -> Mesh {
     Mesh::new(
@@ -93,9 +174,15 @@ pub struct GenerateTerrainEvent {
 struct TerrainGenerationSysParams<'w, 's> {
     commands: Commands<'w, 's>,
     assets: Res<'w, TerrainAssets>,
+    query_voxels: Query<'w, 's, Entity, With<VoxelEntity>>
 }
 
 impl<'w, 's> TerrainGenerationSysParams<'w, 's> {
+    fn despawn_all_voxels(&mut self) {
+        for entity in &self.query_voxels {
+            self.commands.entity(entity).despawn_recursive();
+        }
+    }
     fn spawn_quad(&mut self, transform: Transform, blue: bool) -> Entity {
         let mat = if blue {
             self.assets.blue_mat.clone()
@@ -110,7 +197,7 @@ impl<'w, 's> TerrainGenerationSysParams<'w, 's> {
                     transform,
                     ..default()
                 },
-                CustomUV,
+                VoxelEntity,
             ))
             .id()
     }
@@ -122,44 +209,63 @@ fn sys_generate_terrain(
 ) {
     for ev in voxel_terrain_ev.read() {
         log::info!("regenerating terrain");
+        
+        sys_params.despawn_all_voxels();
+
         // spawn quads at each voxel position
-        for i in ev.terrain.0.iter() {
+        for (index, i) in ev.terrain.0.iter().enumerate() {
+            let (x, y, z): (f32, f32, f32) = (
+                (i.0 * VOXEL_SIZE) as f32,
+                (i.1 * VOXEL_SIZE) as f32,
+                (i.2 * VOXEL_SIZE) as f32,
+            );
+            let tr = Transform::from_xyz(x, y, z);
+            let mut quads_count = 0;
+
             // facing -z
-            {
-                let mut tr = Transform::from_xyz((i.0 * VOXEL_SIZE) as f32, 0.0, 0.0);
+            if !ev.terrain.has_neighbor(index, Direction::Backward) {
+                let mut tr = tr;
                 tr.rotate_y((180.0_f32).to_radians());
                 sys_params.spawn_quad(tr, false);
+                quads_count += 1;
             }
             // facing +z
-            {
-                let mut tr = Transform::from_xyz((i.0 * VOXEL_SIZE) as f32, 0.0, 0.0);
+            if !ev.terrain.has_neighbor(index, Direction::Forward) {
+                let mut tr = tr;
                 tr.rotate_y((0.0_f32).to_radians());
-                sys_params.spawn_quad(tr, false);
+                sys_params.spawn_quad(tr, true);
+                quads_count += 1;
             }
             // facing -y
-            {
-                let mut tr = Transform::from_xyz((i.0 * VOXEL_SIZE) as f32, 0.0, 0.0);
+            if !ev.terrain.has_neighbor(index, Direction::Down) {
+                let mut tr = tr;
                 tr.rotate_x((90.0_f32).to_radians());
-                sys_params.spawn_quad(tr, true);
+                sys_params.spawn_quad(tr, false);
+                quads_count += 1;
             }
             // facing +y
-            {
-                let mut tr = Transform::from_xyz((i.0 * VOXEL_SIZE) as f32, 0.0, 0.0);
+            if !ev.terrain.has_neighbor(index, Direction::Up) {
+                let mut tr = tr;
                 tr.rotate_x((270.0_f32).to_radians());
-                sys_params.spawn_quad(tr, true);
+                sys_params.spawn_quad(tr, false);
+                quads_count += 1;
             }
             // facing +x
-            {
-                let mut tr = Transform::from_xyz((i.0 * VOXEL_SIZE) as f32, 0.0, 0.0);
+            if !ev.terrain.has_neighbor(index, Direction::Right) {
+                let mut tr = tr;
                 tr.rotate_y((90.0_f32).to_radians());
-                sys_params.spawn_quad(tr, true);
+                sys_params.spawn_quad(tr, false);
+                quads_count += 1;
             }
             // facing -x
-            {
-                let mut tr = Transform::from_xyz((i.0 * VOXEL_SIZE) as f32, 0.0, 0.0);
+            if !ev.terrain.has_neighbor(index, Direction::Left) {
+                let mut tr = tr;
                 tr.rotate_y((270.0_f32).to_radians());
-                sys_params.spawn_quad(tr, true);
+                sys_params.spawn_quad(tr, false);
+                quads_count += 1;
             }
+
+            log::debug!("{} quads spawned", quads_count);
         }
     }
 }
@@ -177,25 +283,116 @@ impl Plugin for RenderPlugin {
 #[cfg(test)]
 mod tests {
 
-    use super::{Voxel, VoxelTerrain};
+    use super::{Direction, Voxel, VoxelTerrain};
     #[test]
-    fn test_neighbors() {
+    fn test_x_neighbors() {
         let voxel_terrain = VoxelTerrain(vec![
-            Voxel(-1, 0, 0),
-            Voxel(0, 0, 0),
-            Voxel(1, 0, 0),
-            Voxel(2, 0, 0),
-            Voxel(3, 0, 0),
-            Voxel(5, 0, 0),
+            Voxel(-1, 0, 0), // 0
+            Voxel(0, 0, 0),  // 1
+            Voxel(1, 0, 0),  // 2
+            Voxel(5, 0, 0),  // 3
         ]);
 
-        assert!(voxel_terrain.neighbor_right(0));
-        assert!(!voxel_terrain.neighbor_left(0));
-        assert!(voxel_terrain.neighbor_right(0) && !voxel_terrain.neighbor_left(0));
-        assert!(voxel_terrain.neighbor_right(1) && voxel_terrain.neighbor_left(1));
-        assert!(voxel_terrain.neighbor_right(2) && voxel_terrain.neighbor_left(2));
-        assert!(voxel_terrain.neighbor_right(3) && voxel_terrain.neighbor_left(3));
-        assert!(!voxel_terrain.neighbor_right(4) && voxel_terrain.neighbor_left(4));
-        assert!(!voxel_terrain.neighbor_right(5) && !voxel_terrain.neighbor_left(5));
+        assert!(
+            voxel_terrain.has_neighbor(0, Direction::Right)
+                && !voxel_terrain.has_neighbor(0, Direction::Left)
+        );
+        assert!(
+            voxel_terrain.has_neighbor(1, Direction::Right)
+                && voxel_terrain.has_neighbor(1, Direction::Left)
+        );
+        assert!(
+            !voxel_terrain.has_neighbor(2, Direction::Right)
+                && voxel_terrain.has_neighbor(2, Direction::Left)
+        );
+        assert!(
+            !voxel_terrain.has_neighbor(3, Direction::Right)
+                && !voxel_terrain.has_neighbor(3, Direction::Left)
+        );
+    }
+
+    #[test]
+    fn test_y_neighbors() {
+        let voxel_terrain = VoxelTerrain(vec![
+            Voxel(0, -1, 0), // 0
+            Voxel(0, 0, 0),  // 1
+            Voxel(0, 1, 0),  // 2
+            Voxel(0, 5, 0),  // 3
+        ]);
+
+        assert!(
+            voxel_terrain.has_neighbor(0, Direction::Up)
+                && !voxel_terrain.has_neighbor(0, Direction::Down)
+        );
+        assert!(
+            voxel_terrain.has_neighbor(1, Direction::Up)
+                && voxel_terrain.has_neighbor(1, Direction::Down)
+        );
+        assert!(
+            !voxel_terrain.has_neighbor(2, Direction::Up)
+                && voxel_terrain.has_neighbor(2, Direction::Down)
+        );
+        assert!(
+            !voxel_terrain.has_neighbor(3, Direction::Down)
+                && !voxel_terrain.has_neighbor(3, Direction::Up)
+        );
+    }
+
+    #[test]
+    fn test_z_neighbors() {
+        let voxel_terrain = VoxelTerrain(vec![
+            Voxel(0, 0, -1), // 0
+            Voxel(0, 0, 0),  // 1
+            Voxel(0, 0, 1),  // 2
+            Voxel(0, 0, 5),  // 3
+        ]);
+
+        assert!(
+            voxel_terrain.has_neighbor(0, Direction::Forward)
+                && !voxel_terrain.has_neighbor(0, Direction::Backward)
+        );
+        assert!(
+            voxel_terrain.has_neighbor(1, Direction::Forward)
+                && voxel_terrain.has_neighbor(1, Direction::Backward)
+        );
+        assert!(
+            !voxel_terrain.has_neighbor(2, Direction::Forward)
+                && voxel_terrain.has_neighbor(2, Direction::Backward)
+        );
+        assert!(
+            !voxel_terrain.has_neighbor(3, Direction::Forward)
+                && !voxel_terrain.has_neighbor(3, Direction::Backward)
+        );
+    }
+
+    // diagonals should not be returned as neighbors
+    #[test]
+    fn test_diagonal_neighbors() {
+        {
+            let voxel_terrain = VoxelTerrain(vec![
+                Voxel(0, 0, 0),
+                Voxel(-1, 0, 1),  // left back
+                Voxel(-1, 0, -1), // left front
+                Voxel(1, 0, 1),   // right front
+                Voxel(1, 0, -1),  // right back
+                Voxel(1, 1, 0),   // left top
+                Voxel(-1, 1, 0),  // right top
+                Voxel(0, 1, 1),   // forward top
+                Voxel(0, 1, -1),  // backward top
+                Voxel(-1, -1, 0), // left bottom
+                Voxel(1, -1, 0),  // right bottom
+                Voxel(0, -1, 1),  // forward bottom
+                Voxel(0, -1, -1), // backward bottom
+            ]);
+
+            // no dang neighbors, all alone
+            assert!(!voxel_terrain.has_neighbor(0, Direction::Forward));
+            assert!(!voxel_terrain.has_neighbor(0, Direction::Backward));
+            assert!(!voxel_terrain.has_neighbor(0, Direction::Up));
+            assert!(!voxel_terrain.has_neighbor(0, Direction::Down));
+            assert!(!voxel_terrain.has_neighbor(0, Direction::Left));
+            assert!(!voxel_terrain.has_neighbor(0, Direction::Right));
+            // she's just like me fr
+        }
     }
 }
